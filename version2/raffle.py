a754d12d28a23bca856f86a8a410f8d15442bff3
Yvonne Yeh <yvonneyeh@me.com>
2021-01-14 03:58:13 +0000

    Fix import errors

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,28 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+"""Read customer data from file and run a raffle."""
+
+from customers import get_customers_from_file
+import random
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+if __name__ == '__main__':
+    run_raffle()
